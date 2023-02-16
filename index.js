5da21787d58d826eae4eea4b90d076246bf863a1
Jack DeVries <jdevries3133@gmail.com>
2023-02-15 21:04:09 -0500

    feat: basic pagination solution

--- index.js
+++ index.js
@@ -4,9 +4,46 @@ const app = new express()
 
 app.use(express.static('./public'));
 app.use(express.json())
+app.disable('etag');
+
+
+// Our solution is going to be RESTful, because the data that we send back and
+// forth is going to store the pagination state
+
+/**
+ * The number of items on each page
+ */
+const PAGE_SIZE = 2;
 
 app.get('/api/message', (req, res) => {
-  res.json(messages);
+  const page = typeof req.query.page === 'string'
+    ? parseInt(req.query.page)
+    : 0;
+
+  if (isNaN(page)) {
+    res.status(400).send('Invalid page number')
+  }
+
+  const start = page;
+  const stop = start + PAGE_SIZE;
+  const nextPage = start + PAGE_SIZE;
+  const prevPage = Math.max(start - PAGE_SIZE, 0)
+
+  // normally, a database would do this part - we're going to filter the full
+  // set of messages into the subset that matches our page range
+  //
+  // In SQL, it might look like this:
+  //
+  //     SELECT * FROM messages WHERE id > ${start} and id < ${stop}
+  const msgPage = messages.filter(({id}) => id >= start && id <= stop);
+
+  res.json({
+    data: msgPage,
+    page: {
+      previous: `/api/message?page=${prevPage}`,
+      next: `/api/message?page=${nextPage}`
+    }
+  });
 })
 
 
--- public/script.js
+++ public/script.js
@@ -2,16 +2,55 @@
  * This is in the public folder, so it runs on the client! (in the browser)
  */
 
+let nextUrl = '/api/message';
+let prevUrl = '';
+
+/**
+ * Click handler for the "next page" button
+ */
+async function handleNextPage() {
+  const messages = await getData()
+  putItIntoThemIntoTheDom(messages)
+}
+
+/**
+ * Click handler for the "previous page" button
+ */
+async function handlePrevPage() {
+  const messages = await getData('prev');
+  putItIntoThemIntoTheDom(messages)
+}
+
+/**
+ * Direction can be `next` or `prev`
+ */
+async function getData(direction = 'next') {
+  const response = await fetch(direction === 'next' ? nextUrl : prevUrl);
+  const { data: messages, page } = await response.json();
+
+  nextUrl = page.next;
+  prevUrl = page.prev;
+
+  return messages;
+}
+
+function putItIntoThemIntoTheDom(messages) {
+  document.getElementById("message").innerHTML = `Server says <b>${messages
+    .map(({ msg }) => msg)
+    .join(", ")}</b>`;
+}
+
 /**
  * Note: because of our event listener below, this guy won't run until the
  * page totally finishes loading
  */
 async function main() {
-  const response = await fetch("/api/message");
-  const messages = await response.json();
-  document.getElementById("message").innerText = `Server says ${messages
-    .map(({ msg }) => msg)
-    .join(", ")}`;
+  document.getElementById("next").addEventListener("click", handleNextPage);
+  document.getElementById("prev").addEventListener("click", handlePrevPage);
+
+  // initial fetch
+  const messages = await getData();
+  putItIntoThemIntoTheDom(messages);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
